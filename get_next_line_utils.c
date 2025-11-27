ec75e10eef41168368c9d17892c23a1a5094d919
Matteo <229449054+chauvinmatteo@users.noreply.github.com>
2025-11-27 15:25:53 +0100

    Added header, get_next_line.c and utils.c

---
+++ get_next_line_utils.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/21 14:18:29 by mchauvin          #+#    #+#             */
+/*   Updated: 2025/11/27 15:23:59 by mchauvin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dest[i] = s2[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+void	*ft_memchr(const void *str, int a, size_t n)
+{
+	size_t			i;
+	unsigned char	*s;
+
+	i = 0;
+	s = (unsigned char *)str;
+	while (i < n)
+	{
+		if (s[i] == (unsigned char)a)
+			return ((void *)&s[i]);
+		i++;
+	}
+	return (0);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
