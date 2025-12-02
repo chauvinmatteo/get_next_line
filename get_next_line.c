dd2124db7b49e0e03fd5c94b1fdef40a4798e077
Matteo Chauvin <mchauvin@z4r12p5.42lyon.fr>
2025-12-02 14:18:40 +0100

    Alomst done for all test

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,20 @@
 /*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 13:36:15 by mchauvin          #+#    #+#             */
-/*   Updated: 2025/11/29 13:08:41 by mchauvin         ###   ########.fr       */
+/*   Updated: 2025/12/02 14:17:06 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static int	is_last_line_empty(char *stash, int ret)
+{
+	if (!stash)
+		return (1);
+	if (stash[0] == '\0' && ret == 0)
+		return (1);
+	return (0);
+}
 
 char	*get_next_line(int fd)
 {
@@ -21,7 +30,7 @@ char	*get_next_line(int fd)
 	char		*buffer;
 
 	line = NULL;
-	if (fd == -1 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
@@ -32,24 +41,36 @@ char	*get_next_line(int fd)
 		ret = read(fd, buffer, BUFFER_SIZE);
 		if (ret == -1)
 		{
-			free(stash);
-			free (buffer);
+			free(buffer);
+			if (stash)
+			{
+				free(stash);
+				stash = NULL;
+			}
 			return (NULL);
 		}
 		else if (ret > 0)
 		{
 			buffer[ret] = '\0';
-			newstash = ft_strjoin(stash, buffer);
-			if (stash)
-				free(stash);
-			stash = newstash;
+			newstash = stash;
+			stash = ft_strjoin(stash, buffer);
+			free(newstash);
 		}
 	}
-	free (buffer);
-	if (!stash)
+	free(buffer);
+	if (is_last_line_empty(stash, ret))
+	{
+		free(stash);
+		stash = NULL;
 		return (NULL);
+	}
 	line = ft_extract_line(stash);
 	stash = ft_clean_stash(stash);
+	if (stash && stash[0] == '\0')
+	{
+		free(stash);
+		stash = NULL;
+	}
 	return (line);
 }
 
@@ -58,7 +79,7 @@ char	*get_next_line(int fd)
 
 // int	main(void)
 // {
-// 	int fd = open("test.txt", O_RDONLY);
+// 	int fd = open("test1.txt", O_RDONLY);
 // 	char *line;
 
 // 	if (fd == -1)
