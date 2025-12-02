66647a2124b52fefc67e34d8fd4bbd7659460632
Matteo Chauvin <mchauvin@z4r12p5.42lyon.fr>
2025-12-02 17:57:13 +0100

    change some code and made it norm ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,70 @@
 /*   By: mchauvin <mchauvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 13:36:15 by mchauvin          #+#    #+#             */
-/*   Updated: 2025/12/02 15:14:56 by mchauvin         ###   ########.fr       */
+/*   Updated: 2025/12/02 17:56:06 by mchauvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+static char	*ft_clean_stash(char *stash)
 {
-	int			ret;
-	static char	*stash;
-	char		*tmp;
-	char		*line;
-	char		*buffer;
+	int		i;
+	int		j;
+	char	*new_stash;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (!stash)
 		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+		return (free(stash), NULL);
+	i++;
+	if (!stash[i])
+		return (free(stash), NULL);
+	new_stash = malloc(sizeof(char) * (ft_strlen(stash + i) + 1));
+	if (!new_stash)
+		return (NULL);
+	j = 0;
+	while (stash[i])
+		new_stash[j++] = stash[i++];
+	new_stash[j] = '\0';
+	free(stash);
+	return (new_stash);
+}
+
+static char	*ft_extract_line(char *stash)
+{
+	int		i;
+	int		j;
+	char	*new_line;
+
+	if (!stash)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	new_line = malloc(sizeof(char) * (i + 1));
+	if (!new_line)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		new_line[j] = stash[j];
+		j++;
+	}
+	new_line[j] = '\0';
+	return (new_line);
+}
+
+static char	*read_and_update_stash(int fd, char *stash)
+{
+	char	*buffer;
+	int		ret;
+
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
@@ -32,42 +80,49 @@ char	*get_next_line(int fd)
 		if (ret == -1)
 		{
 			free(buffer);
-			if (stash)
-			{
-				free(stash);
-				stash = NULL;
-			}
+			free(stash);
 			return (NULL);
 		}
-		else if (ret > 0)
+		if (ret > 0)
 		{
 			buffer[ret] = '\0';
-			tmp = stash;
-			stash = ft_strjoin(stash, buffer);
-			free(tmp);
+			stash = ft_strjoin_free(stash, buffer);
 		}
 	}
 	free(buffer);
+	return (stash);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = read_and_update_stash(fd, stash);
+	if (!stash)
+		return (NULL);
 	line = ft_extract_line(stash);
 	stash = ft_clean_stash(stash);
 	return (line);
 }
 
-#include <fcntl.h>
-#include <stdio.h>
+// #include <fcntl.h>
+// #include <stdio.h>
 
-int	main(void)
-{
-	int fd = open("test1.txt", O_RDONLY);
-	char *line;
+// int	main(void)
+// {
+// 	int fd = open("test1.txt", O_RDONLY);
+// 	char *line;
 
-	if (fd == -1)
-		return (0);
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("%s\n", line);
-		free(line);
-	}
-	close(fd);
-	return (0);
-}
+// 	if (fd == -1)
+// 		return (0);
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s\n", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
